a88e25bd6bc6b8563ed9a33874db11479accead0
multics76 <48181980+multics76@users.noreply.github.com>
2024-01-19 22:35:58 +0100

    Update install-termux.sh

--- install-termux.sh
+++ install-termux.sh
@@ -8,7 +8,7 @@ pkg install --yes python python-pip &&
 if [ -d "${path}" ]; then
     rm -rf --interactive=once "${path}"
 fi &&
-curl -fsSLo "${download_path}" 'https://github.com/monosans/proxy-scraper-checker/archive/refs/heads/main.zip' &&
+curl -fsSLo "${download_path}" 'https://github.com/multics76/proxy-scraper-checker/archive/refs/heads/main.zip' &&
 unzip -d "${base_path}" "${download_path}" &&
 mv "${path}-main" "${path}" &&
 python3 -m pip install -U --no-cache-dir --disable-pip-version-check setuptools wheel &&
